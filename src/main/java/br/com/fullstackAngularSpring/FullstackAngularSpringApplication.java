ca548f828d4240699c536b1e59dcbc57a2737911
Bruno Batista <brunno188@hotmail.com>
2018-04-05 23:40:10 -0300

    spring boot with angular 4 started new project

---
+++ src/main/java/br/com/fullstackAngularSpring/FullstackAngularSpringApplication.java
@@ -0,0 +1,12 @@
+package br.com.fullstackAngularSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackAngularSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackAngularSpringApplication.class, args);
+	}
+}
