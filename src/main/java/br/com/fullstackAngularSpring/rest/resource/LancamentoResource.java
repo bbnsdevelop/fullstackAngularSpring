88ead0f5879e723fdf7fbc5c40e8aeb95ac574a1
codeit18 (Bruno Batista do Nascimento Santos) <CODEIT18@fornecedor.gamasaude.com.br>
2018-05-28 17:28:34 -0300

    busca com paginação

--- src/main/java/br/com/fullstackAngularSpring/rest/resource/LancamentoResource.java
+++ src/main/java/br/com/fullstackAngularSpring/rest/resource/LancamentoResource.java
@@ -1,11 +1,13 @@
 package br.com.fullstackAngularSpring.rest.resource;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
 import br.com.fullstackAngularSpring.rest.dataTransferObject.LancamentoDto;
 
 public interface LancamentoResource {
 	ResponseEntity<?> buscaLancamentoPorPessoa(Long id);
+	ResponseEntity<?> buscaLancamentosPaginacao(Long id, Pageable page);
 	ResponseEntity<?> deletarLancamento(Long id);
 	ResponseEntity<?> buscaLancamentos();
 	ResponseEntity<?> salvarLancamento(LancamentoDto request);
